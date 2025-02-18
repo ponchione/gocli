1824ecd0d2de425d7bc9f0b0d3fd8a9b185a9cc4
Mitchell Ponchione <mitchell.ponchione@protonmail.com>
2025-02-18 04:56:17 -0500

    Adjusted the downloads organizer

--- cmd/orgdownloads.go
+++ cmd/orgdownloads.go
@@ -21,14 +21,25 @@ func init() {
 	}
 }
 
-var extensions = map[string]bool{
-	".zip":  true,
-	".png":  true,
-	".jpeg": true,
-	".jpg":  true,
-	".msi":  true,
-	".txt":  true,
-	".pdf":  true,
+var extensions = map[string]string{
+	".zip":  "zip_files",
+	".png":  "image_files",
+	".jpeg": "image_files",
+	".jpg":  "image_files",
+	".msi":  "installers",
+	".txt":  "text_files",
+	".pdf":  "pdf_files",
+}
+
+var folderNames = []string{
+	"zip_files",
+	"image_files",
+	"pdf_files",
+	"old_files",
+	"installers",
+	"csv_files",
+	"spreadsheets",
+	"text_files",
 }
 
 func (o *OrganizeCommand) Execute(args []string) error {
@@ -38,6 +49,7 @@ func (o *OrganizeCommand) Execute(args []string) error {
 	}
 
 	downloadsPath := filepath.Join(homeDir, "Downloads")
+	createFolders(downloadsPath)
 
 	contents, err := os.ReadDir(downloadsPath)
 	if err != nil {
@@ -50,26 +62,18 @@ func (o *OrganizeCommand) Execute(args []string) error {
 		}
 
 		ext := strings.ToLower(filepath.Ext(file.Name()))
-		if !extensions[ext] {
+		if _, exists := extensions[ext]; !exists {
+			log.Printf("extension for %s does not exist for sorting\n", file)
 			continue
 		}
 
-		extName := ext[1:]
-		targetDir := filepath.Join(downloadsPath, extName+"_files")
-		if err := os.MkdirAll(targetDir, 0755); err != nil {
-			log.Printf("error creating directory for %s: %v\n", extName, err)
-			continue
-		}
-
+		targetDir := filepath.Join(downloadsPath, extensions[ext])
 		sourcePath := filepath.Join(downloadsPath, file.Name())
 		destPath := filepath.Join(targetDir, file.Name())
 		if err := moveFile(sourcePath, destPath); err != nil {
 			log.Printf("error moving file %s: %v", file.Name(), err)
 			continue
 		}
-
-		fmt.Printf("Moved %s to %s\n", file.Name(), filepath.Base(targetDir))
-
 	}
 
 	fmt.Println("Sorting completed.")
@@ -100,6 +104,19 @@ func moveFile(sourcePath string, destPath string) error {
 	}
 
 	return nil
+}
+
+func createFolders(dPath string) {
+	for _, name := range folderNames {
+		target := filepath.Join(dPath, name)
+		err := os.MkdirAll(target, 0755)
+		if err != nil {
+			log.Printf("error creating directory for %s: %v\n", name, err)
+			continue
+		} else {
+			log.Printf("folder %s already exists\n", name)
+		}
+	}
 }
 
 func (o *OrganizeCommand) Help() string {
